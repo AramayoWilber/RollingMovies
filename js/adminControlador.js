065b542c46a5aac93b0f68e3e668a49efbf9c79d
aramayowil <aramayowil@gmail.com>
2024-03-14 21:05:01 -0300

    fontedTerminado

--- js/adminControlador.js
+++ js/adminControlador.js
@@ -14,9 +14,9 @@ const llenarTabla = () => {
         let peliculaDestacada;
 
         if(element.publicado){
-            peliculaPulicada = `<button class="btn-publicado"><i class="bi bi-check-square"></i></button>`;
+            peliculaPulicada = `<button class="btn-publicado"><i class="fa-solid fa-circle-check"></i></button>`;
         }else
-            peliculaPulicada = `<button class="btn-no-publicado"><i class="bi bi-x-circle"></i></button>`;
+            peliculaPulicada = `<button class="btn-no-publicado"><i class="fa-solid fa-circle-xmark"></i></button>`;
 
         if(element.destacada){
             peliculaDestacada = `<button class="btn-destacado" id="btn_destacado_${element.codigo}"><i class="bi bi-star-fill"></i></button>`;
@@ -25,15 +25,15 @@ const llenarTabla = () => {
     
 
         const filaTabla = `
-        <th scope="row" name="hola" id="id_fila_${element.codigo}">${element.codigo}</th>
+        <th scope="row" class="text-center" id="id_fila_${element.codigo}">${element.codigo}</th>
         <td class="p-2">${element.nombre}</td>
         <td>${element.categoria}</td>
         <td>
             ${peliculaPulicada}
         </td>
         <td>
-            <button class="btn-opciones" data-bs-toggle="modal" data-bs-target="#modal-delete"><i class="bi bi-trash"></i></button>
-            <button class="btn-opciones" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-pencil-square"></i></button>
+            <button class="btn-opcion-borrar" data-bs-toggle="modal" data-bs-target="#modal-delete"><i class="bi bi-trash"></i></button>
+            <button class="btn-opcion-editar" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-pencil-square"></i></button>
             ${peliculaDestacada}
         </td>
         `;
